a72672eebf8557f421d5429398b625c6b390cd22
Alumno <alumno@admins-macbook-pro.local>
2017-02-07 20:52:23 -0500

    Antes del recreo

---
+++ Transiciones en iOS/ShowDetailViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ShowDetailViewController.swift
+//  Transiciones en iOS
+//
+//  Created by Alumno on 7/02/17.
+//  Copyright © 2017 alumno. All rights reserved.
+//
+
+import UIKit
+
+class ShowDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func btnRegresar(_ sender: UIButton) {
+   
+        self.dismiss(animated: true){
+            
+        }
+        
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
